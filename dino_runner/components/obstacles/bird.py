62145a645c36ab6868c97f269dd32c287699be8d
GarridoLacerda <132011026+GarridoLacerda@users.noreply.github.com>
2023-05-06 00:01:18 -0300

    .

--- dino_runner/components/obstacles/bird.py
+++ dino_runner/components/obstacles/bird.py
@@ -9,6 +9,8 @@ class Bird(Obstacle):
         self.animation_speed = 5
         self.animation_counter = 0
         self.rect.y = random.randrange(100, 285, 10)
+        self.direction = 1 # 1 para descer, -1 para subir
+        self.speed = 5
 
     def update(self, game_speed, obstacles):
         self.animation_counter += 1
@@ -16,6 +18,13 @@ class Bird(Obstacle):
             self.image_index = (self.image_index + 1) % len(self.images)
             self.image = self.images[self.image_index]
             self.animation_counter = 0
+
+        # Ajusta a posição vertical do obstáculo
+        self.rect.y += self.speed * self.direction
+        if self.rect.top <= 100:
+            self.direction = 1
+        elif self.rect.bottom >= 285:
+            self.direction = -1
 
         super().update(game_speed, obstacles)
 
